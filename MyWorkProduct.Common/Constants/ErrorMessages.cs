91408d0ef0edbd352a655b089fe032fa6c6044b0
joemerdizon <joemer.dizon@gmail.com>
2022-05-18 23:50:36 +0800

    refactor: improve auth end point

---
+++ MyWorkProduct.Common/Constants/ErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorkProduct.Common.Constants
+{
+    public static class ErrorMessages
+    {
+        public static readonly string INVALID_USER = "Invalid User";
+    }
+}
---
+++ MyWorkProduct.Common/Helpers/AuthenticateHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorkProduct.Common.Helpers
+{
+    public static class AuthenticateHelper
+    {
+        public static bool VerifyHashedString(string hashedValue, string userEnteredValue)
+        {
+            bool flag = false;
+            flag = BCrypt.Net.BCrypt.Verify(userEnteredValue + "^M8~YY", hashedValue);
+            return flag;
+        }
+    }
+}
